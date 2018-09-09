be0c5d2781d3776cf2e0eb2d9c0cdd145e89515f
Saurabh <1saurabh.desai@gmail.com>
2018-09-09 14:13:01 +0530

    added dependencies and setup project

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import './src/config/ReactronConfig';
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('alphaOnline', () => App);
+import {name as appName} from './app.json';
+
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+console.ignoredYellowBox = ['Remote debugger'];
+
+
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+export default class extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
